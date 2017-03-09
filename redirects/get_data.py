9bf7e9932094d6b2643baefe6d153e93d6287453
staeiou <sgeiger@gmail.com>
2017-03-09 00:50:50 +0000

    expanded analysis, add quarry links

--- redirects/get_data.py
+++ redirects/get_data.py
@@ -1,29 +1,38 @@
+# Script for parsing a list of redirects with endashes and getting data
+# about when the page was first edited and by whom
 
 import pywikibot
 site = pywikibot.Site('en', 'wikipedia')
 
-
-# In[79]:
-
 import pandas as pd
 import numpy as np
 import seaborn as sns
-
 import pickle
 
 
-# In[31]:
+# This query on Quarry: https://quarry.wmflabs.org/query/17182
+
+"""
+select page_id, page_title, rd_title from redirect 
+inner join page on rd_from = page_id
+where rd_namespace = 0 and rd_title LIKE '%–%'
+"""
 
 redirects_df = pd.read_csv("enwiki-redirects-endash-20170308.tsv", sep="\t")
 len(redirects_df)
 
-
-# In[82]:
-
 error_count = 0
-total_df = pd.DataFrame(columns=["revid", "timestamp", "user", "comment", "is_redirect", "page_title", "page_namespace", "page_text"])
+total_df = pd.DataFrame(columns=["revid",
+                                 "timestamp",
+                                 "user",
+                                 "comment",
+                                 "is_redirect",
+                                 "page_title",
+                                 "page_namespace",
+                                 "page_text"])
 count = 0 
 errors = []
+
 for r in redirects_df.iterrows():
     revs = []
     redirect_page = r[1].page_title
@@ -35,7 +44,15 @@ for r in redirects_df.iterrows():
         errors.append(r[1].page_title)
         error_count = error_count + 1
 
-    rev_df = pd.DataFrame(revs, columns=["revid", "timestamp", "user", "comment", "is_redirect", "page_title", "page_namespace", "page_text"]) 
+    rev_df = pd.DataFrame(revs, columns=["revid",
+                                         "timestamp",
+                                         "user",
+                                         "comment",
+                                         "is_redirect",
+                                         "page_title",
+                                         "page_namespace",
+                                         "page_text"]) 
+    
     total_df = pd.concat([total_df, rev_df])
     
     count = count + 1
